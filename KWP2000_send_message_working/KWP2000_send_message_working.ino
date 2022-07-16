f82ce4739e9cac5b28e22b76ec500cfe485a27e0
salvatoreBosco <salvatorebosco.mail@gmail.com>
2022-07-16 15:29:20 +0200

    fix bug

--- KWP2000_send_message_working/KWP2000_send_message_working.ino
+++ KWP2000_send_message_working/KWP2000_send_message_working.ino
@@ -2,7 +2,7 @@
   KWP2000 request pid 
 */
 
-#include <SoftwareSerial.h>
+#include "..\kwp2000.h"
 
 SoftwareSerial mS(10, 11); // RX, TX
 
@@ -13,12 +13,7 @@ void clearMS();
 void clearSerial(); // Pulisce il buffer della seriale
 float formula(char);
 
-int mode = -1;
-char pid = -1;
-char startComm[] = {0xc1, 0x33, 0xf1, 0x81, 0x66};
 
-
-char bytePID[64];
 
 int i;
 
@@ -59,122 +54,7 @@ void loop() {
 }
 
 
-
-float kwp2000_request(int mode, char pid) {
-
-  
-  kwp2000_fastInit(); //inizializza la comunicazione
-
-  /* Send byte */
-  
-  if(mode == 1){
-    switch(pid){
-      case 0:
-        break;
-      case 1:
-        break;
-      case 2:
-        break;
-      case 3:
-        break;
-      case 4:
-        break;
-      case 5:
-        break;
-      case 6:
-        break;
-      case 7:
-        break;
-      case 8:
-        break;
-      case 9:
-        break;
-      case 10:
-        break;
-      case 11:
-        break;
-      case 12:
-        /* Send byte for speed */
-        for (i = 0; i < 6; i++) {
-          mS.write(PIDlist[(int)pid][i]);
-          delay(6);
-        }
-        break;
-      case 13:
-        /* Send byte for rpm */ 
-        for (i = 0; i < 6; i++) {
-          mS.write(PIDlist[(int)pid][i]);
-          delay(6);
-        }  
-        break;
-      default:
-        return -1.1;
-    }
-  }
-  clearMS();
-  i = 0;
-  
-  /* raccolta dati */
-  
-  while (mS.available() < 1 ) {Serial.print(" ");}
-  Serial.println("");
-  while (mS.available() > 0) {
-    bytePID[i] = mS.read();
-    i++; 
-  }
-  return formula(pid);
-}
-
-void clearMS() {
-  mS.end();
-  mS.begin(10400);
-}
-
 void clearSerial() {
   Serial.end();
   Serial.begin(9600);
-}
-
-float formula(char pid){
-  /*calcola e mostra risultato*/
-  switch(pid){
-      case 0:
-        break;
-      case 1:
-        break;
-      case 2:
-        break;
-      case 3:
-        break;
-      case 4:
-        break;
-      case 5:
-        break;
-      case 6:
-        break;
-      case 7:
-        break;
-      case 8:
-        break;
-      case 9:
-        break;
-      case 10:
-        break;
-      case 11:
-        break;
-      case 12:
-        Serial.print("speed = ");
-        Serial.print(bytePID[5], DEC);
-        Serial.println(" km/h");
-        return bytePID[5];
-        break;
-      case 13:
-        Serial.print("rpm = ");
-        Serial.print( (((bytePID[5]*256)+bytePID[6])/4) , DEC);
-        Serial.println(" giro/minuto");
-        return ((bytePID[5]*256)+bytePID[6])/4;
-        break;
-      case 14:
-        break;
-    }
 }
